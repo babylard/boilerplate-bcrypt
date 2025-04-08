f47a148f5e6c9c52e634f747d37cdcfd11ebf55e
Lardsonian Chiozza <babylard1@protonmail.com>
2025-04-08 17:38:03 +0000

    Finish bcrypt

--- server.js
+++ server.js
@@ -2,23 +2,23 @@
 const express     = require('express');
 const bodyParser  = require('body-parser');
 const fccTesting  = require('./freeCodeCamp/fcctesting.js');
+const bcrypt = require('bcrypt')
 const app         = express();
 fccTesting(app);
 const saltRounds = 12;
 const myPlaintextPassword = 'sUperpassw0rd!';
 const someOtherPlaintextPassword = 'pass123';
 
-
 //START_ASYNC -do not remove notes, place code between correct pair of notes.
-
-
-
+  
 //END_ASYNC
 
 //START_SYNC
+let hash = bcrypt.hashSync(myPlaintextPassword, saltRounds);
+let result = bcrypt.compareSync(myPlaintextPassword, hash);
 
-
-
+console.log(hash);
+console.log(result);
 //END_SYNC
 
 
